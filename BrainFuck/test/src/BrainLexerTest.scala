49758596a1ca87ecd9f344c506acca2815b69bd4
Bartosz Buczek <bartosz.buczek@u2i.com>
2026-03-24 19:29:21 +0100

    whitespace handling test

--- BrainFuck/test/src/BrainLexerTest.scala
+++ BrainFuck/test/src/BrainLexerTest.scala
@@ -8,4 +8,15 @@ class BrainLexerTest extends AnyFunSuite {
     val expectedNames = List("read", "l_bracket", "next", "inc", "print", "prev", "dec", "r_bracket")
     assert(result.map(_.name) == expectedNames)
   }
+
+  test("tokenize should ignore whitespace in Brainfuck code") {
+    val codeWithWhitespace = """
+    ,[
+      > + . < - 
+    ]
+    """
+    val (_, result) = BrainLexer.tokenize(codeWithWhitespace)
+    val expectedNames = List("read", "l_bracket", "next", "inc", "print", "prev", "dec", "r_bracket")
+    assert(result.map(_.name) == expectedNames)
+  }
 }
