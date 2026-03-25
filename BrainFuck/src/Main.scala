9698a22010978b9648172919d1e10419d80ee67a
Bartosz Buczek <bartosz.buczek@u2i.com>
2026-03-25 12:06:54 +0100

    full hello world

--- BrainFuck/src/Main.scala
+++ BrainFuck/src/Main.scala
@@ -1,7 +1,12 @@
 package BrainFuck
 
+import alpaca.*
+
 @main def run(input: String): Unit = {
-  val (final_ctx, _tokens) = BrainLexer.tokenize(input)
+  val (final_ctx, tokens) = BrainLexer.tokenize(input)
   if final_ctx.brackets != 0 then
     throw IllegalStateException(s"Unclosed bracket at line ${final_ctx.line}, position ${final_ctx.position}")
+
+  val (_, ast) = BrainParser.parse(tokens)
+  ast.eval()
 }
--- BrainFuck/test/src/MainTest.scala
+++ BrainFuck/test/src/MainTest.scala
@@ -1,6 +1,7 @@
 package BrainFuck
 
 import org.scalatest.funsuite.AnyFunSuite
+import java.io.ByteArrayOutputStream
 
 class MainTest extends AnyFunSuite {
   test("main should throw on unclosed bracket") {
@@ -8,5 +9,16 @@ class MainTest extends AnyFunSuite {
       BrainFuck.run(".[+")
     }
     assert(exception.getMessage == "Unclosed bracket at line 1, position 4")
+  }
+
+  test("main should execute hello world correctly") {
+    val input = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++."
+
+    val out = ByteArrayOutputStream()
+
+    Console.withOut(out):
+      BrainFuck.run(input)
+
+    assert(out.toString == "Hello World!\n")
   }
 }
