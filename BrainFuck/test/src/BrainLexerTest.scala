22055bd62635fbe9efe0fd63352bf8b24d2dcaa6
Bartosz Buczek <bartosz.buczek@u2i.com>
2026-03-25 07:11:27 +0100

    first lexer test

---
+++ BrainFuck/test/src/BrainLexerTest.scala
@@ -0,0 +1,11 @@
+package BrainFuck
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class BrainLexerTest extends AnyFunSuite {
+  test("tokenize should parse Brainfuck code correctly") {
+    val (_, result) = BrainLexer.tokenize(",[>+.<-]")
+    val expectedNames = List("read", "l_bracket", "next", "inc", "print", "prev", "dec", "r_bracket")
+    assert(result.map(_.name) == expectedNames)
+  }
+}
