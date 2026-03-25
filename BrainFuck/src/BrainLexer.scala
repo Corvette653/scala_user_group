3ff9758688ea64043a0b528fafef1d15377629b0
Bartosz Buczek <bartosz.buczek@u2i.com>
2026-03-25 07:03:53 +0100

    identifying mismatched brackets

--- BrainFuck/src/BrainLexer.scala
+++ BrainFuck/src/BrainLexer.scala
@@ -2,14 +2,22 @@ package BrainFuck
 
 import alpaca.*
 
-val BrainLexer = lexer:
+case class BrainLexerCtx(var brackets: Int = 0) extends LexerCtx
+
+val BrainLexer = lexer[BrainLexerCtx]:
   case ">" => Token["next"]
   case "<" => Token["prev"]
   case "\\+" => Token["inc"]
   case "-" => Token["dec"]
   case "\\." => Token["print"]
   case "," => Token["read"]
-  case "\\[" => Token["l_bracket"]
-  case "\\]" => Token["r_bracket"]
+  case "\\[" => 
+    ctx.brackets += 1
+    Token["l_bracket"]
+  case "\\]" => 
+    ctx.brackets -= 1
+    if ctx.brackets < 0 then
+      throw IllegalStateException(s"Unmatched bracket at line ???, position ???")
+    Token["r_bracket"]
   case "\n" => Token.Ignored
   case "." => Token.Ignored
