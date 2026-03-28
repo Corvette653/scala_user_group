3e4997698f9ebb665b2730960c242dabb581bbd5
Bartosz Buczek <bartosz.buczek@u2i.com>
2026-03-28 11:14:18 +0100

    brackets validation v1

--- BrainFuck/src/BrainLexer.scala
+++ BrainFuck/src/BrainLexer.scala
@@ -1,15 +1,22 @@
 package BrainFuck
 
 import alpaca.*
+case class BrainLexerCtx(var openBrackets: Int = 0) extends LexerCtx
 
-val BrainLexer = lexer:
+val BrainLexer = lexer[BrainLexerCtx]:
   case ">" => Token["next"]
   case "<" => Token["prev"]
   case "\\+" => Token["inc"]
   case "-" => Token["dec"]
   case "\\." => Token["print"]
   case "," => Token["read"]
-  case "\\[" => Token["l_bracket"]
-  case "\\]" => Token["r_bracket"]
+  case "\\[" =>
+    ctx.openBrackets += 1
+    Token["l_bracket"]
+  case "\\]" =>
+    ctx.openBrackets -= 1
+    if ctx.openBrackets < 0 then
+      throw IllegalStateException(s"Unmatched closing bracket")
+    Token["r_bracket"]
   case "\n" => Token.Ignored
   case "." => Token.Ignored
